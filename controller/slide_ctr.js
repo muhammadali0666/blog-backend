d8235ceeb84b0a170385328dc52ea0458b5898d7
muhammadali0666 <muhammadalishuhratjonov50@gmail.com>
2024-02-09 17:37:35 +0500

    added edit function to slide apis

--- controller/slide_ctr.js
+++ controller/slide_ctr.js
@@ -10,13 +10,13 @@ const createSlide = async (req, res) => {
   try {
     const { img } = req.body;
 
-    if(!img) {
+    if (!img) {
       return res.send({
-        message: "slide not found"
-      })
+        message: "slide not found",
+      });
     }
 
-    await Slides.create({img: img });
+    await Slides.create({ img: img });
 
     return res.status(200).send({
       message: "created slide",
@@ -27,6 +27,31 @@ const createSlide = async (req, res) => {
     });
   }
 };
+
+const editSlide = async (req, res) => {
+  try {
+    const { img } = req.body;
+    const { id } = req.params;
+
+    const updatedImg = await Slides.update(
+      { img },
+      {
+        returning: true,
+        plain: false,
+        where: {
+          id,
+        },
+      }
+    );
+
+    return res.send(updatedImg.filter((e) => e));
+  } catch (error) {
+    return res.send({
+      message: error.message,
+    });
+  }
+};
+
 const deleteSlide = async (req, res) => {
   try {
     const { id } = req.params;
@@ -59,5 +84,6 @@ const deleteSlide = async (req, res) => {
 module.exports = {
   getSlides,
   createSlide,
-  deleteSlide
+  editSlide,
+  deleteSlide,
 };
--- routes/slide.js
+++ routes/slide.js
@@ -1,11 +1,12 @@
 const {Router} = require("express")
-const {getSlides, createSlide, deleteSlide} = require("../controller/slide_ctr")
+const {getSlides, createSlide, deleteSlide, editSlide} = require("../controller/slide_ctr")
 const { verifyAdmin } = require("../middleware/auth_middleware")
 
 const slideRouter = Router()
 
 slideRouter.get("/slide", getSlides)
 slideRouter.post("/create_slide", verifyAdmin, createSlide)
+slideRouter.put("/edit_slide/:id", verifyAdmin, editSlide)
 slideRouter.delete("/delete_slide/:id", verifyAdmin, deleteSlide)
 
 module.exports = slideRouter
